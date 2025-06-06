f35c168602c0ac2986d9617f9e44f9499c5d1519
arieljcs <arieljesuscuevassenger@gmail.com>
2025-06-05 21:03:56 -0300

    Create app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html> 
+<html lang="es"> 
+<head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>@yield('titulo')</title> 
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> 
+    
+</head> 
+<body> 
+    <div class="container mt-4"> 
+        @yield('contenido') 
+    </div> 
+<!-- JS de Bootstrap --> 
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script> 
+</body> 
+</html>
