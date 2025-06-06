60c91188f970b3e1730e3351f2fd6a74e63c0f4a
arieljcs <arieljesuscuevassenger@gmail.com>
2025-06-05 21:07:39 -0300

    actualizacion layouts.app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
     </div> 
 <!-- JS de Bootstrap --> 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script> 
 </body> 
 </html>
