261ae243addaadcd6cb158c647740ac56073bf6a
Roman <romansola123@gmail.com>
2025-06-25 19:30:32 -0300

    se modificarion las camas

--- resources/views/camas/index.blade.php
+++ resources/views/camas/index.blade.php
@@ -13,8 +13,48 @@
         </a>
     </div>
 
-    <div class="bg-white shadow-md rounded-lg overflow-auto border border-gray-200">
-        <table class="min-w-full table-auto text-sm text-gray-800">
+    <div class="row" style="width: 100%">  
+        @php
+            $cont = 1;
+        @endphp
+        @while ($cont <= 8) 
+            <div class="container col-3 shadow" style="background-color: #fff; border-radius: 22px; border: solid 1px lightgreen; padding: 10px; margin: 2%; text-align: center;">
+                <h3>Habitacion {{ $cont }}</h3>
+                <div class="card" style="width: 18rem; border-radius: 12px; background-color: #e7e7e7;; margin-bottom: 12px;">
+                    <div class="card-body">
+                        <div class="row mb-2">
+                            <div class="col-7">
+                                <h5 class="card-title"><b>cama /numero/</b></h5>
+                            </div>
+                            <div class="col-4">
+                                <div style="text-align: center; border-radius: 4px; background-color: #cacfd2;">Estado</div>
+                            </div>
+                        </div>
+                        <h4 class="mb-2">LIBRE/OCUPADO</h4>
+                        <button class="btn"  style="background-color: lightgreen">Asignar paciente</button>
+                    </div>
+                </div>
+                <div class="card" style="width: 18rem; border-radius: 12px; background-color: #e7e7e7;">
+                    <div class="card-body">
+                        <div class="row mb-2">
+                            <div class="col-7">
+                                <h5 class="card-title"><b>cama /numero/</b></h5>
+                            </div>
+                            <div class="col-4">
+                                <div style="text-align: center; border-radius: 4px; background-color: #cacfd2;">Estado</div>
+                            </div>
+                        </div>
+                        <h4 class="mb-2">LIBRE/OCUPADO</h4>
+                        <button class="btn" style="background-color: lightgreen">Asignar paciente</button>
+                    </div>
+                </div>
+            </div>
+            
+        @php
+            $cont++;
+        @endphp
+        @endwhile
+        {{-- <table class="min-w-full table-auto text-sm text-gray-800">
             <thead class="bg-green-100 text-green-800 font-semibold">
                 <tr>
                     <th class="px-4 py-2 border">Código</th>
@@ -45,7 +85,7 @@
                 </tr>
                 @endforelse
             </tbody>
-        </table>
+        </table> --}}
     </div>
 </div>
 @endsection
