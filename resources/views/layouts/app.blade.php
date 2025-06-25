887f445be661c31a09f17de1e569631901fffe92
Yeison <yeison.araujo.1995@hotmail.com>
2025-06-25 20:19:00 -0300

    Resuelto conflicto en App

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.2/dist/js/bootstrap.bundle.min.js"></script>
 
+
     <script src="https://cdn.tailwindcss.com"></script>
 
     <!-- Lucide Iconos -->
@@ -62,4 +63,5 @@
 
 </body>
 </html>
+
 
