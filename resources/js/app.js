add6a209ec6a1b9bd839e6333f284f2f47a8d65d
Roman Sola <147886030+RomanSola@users.noreply.github.com>
2025-09-27 22:07:07 -0300

    se agrego breeze y admin a todas las vistas

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,7 @@
 import './bootstrap';
-    const menuBtn = document.getElementById('menuBtn');
-    const dropdown = document.getElementById('dropdownMenu');
 
-    menuBtn.addEventListener('click', () => {
-      dropdown.classList.toggle('hidden');
-    });
+import Alpine from 'alpinejs';
 
-    window.addEventListener('click', (e) => {
-      if (!menuBtn.contains(e.target) && !dropdown.contains(e.target)) {
-        dropdown.classList.add('hidden');
-      }
-    });
- 
+window.Alpine = Alpine;
+
+Alpine.start();
