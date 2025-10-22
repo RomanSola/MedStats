ba75be1b1277624acfc0564809466980a8efbc5e
YonelAraujo <yonelaraujo2000@gmail.com>
2025-10-22 20:40:37 -0300

    "me daba error de validacion en camas, igual esta comentado"

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -15,15 +15,19 @@ class InicioController extends Controller
     public function index() //Pagina inicial
     {
         //Cantidad de pacientes
-        $pacientes = count( Paciente::where('habitacion_id', '!=', null )->where('cama_id', '!=', null )->get() );
-        
+        $pacientes = count(Paciente::where('habitacion_id', '!=', null)->where('cama_id', '!=', null)->get());
+
         //Porcentaje de camas
-        $camasOcupadas = count( Cama::where('ocupada', 1 )->get() );
-        $camasTotales = count( Cama::all() );
-        $porcentajeCamas = intval( ( $camasOcupadas * 100 ) / $camasTotales );
+        $camasOcupadas = count(Cama::where('ocupada', 1)->get());
+        $camasTotales = count(Cama::all());
+        // $porcentajeCamas = intval( ( $camasOcupadas * 100 ) / $camasTotales );
+        $porcentajeCamas = $camasTotales > 0
+            ? intval(($camasOcupadas * 100) / $camasTotales)
+            : 0;
+
 
         //Cantidad de Cirugías
-        $cantCirugias = count( Cirugia::whereYear('fecha_cirugia', date('Y'))->get() );
+        $cantCirugias = count(Cirugia::whereYear('fecha_cirugia', date('Y'))->get());
 
         return view('index', compact('pacientes', 'porcentajeCamas', 'cantCirugias')); //Llama a la vista y le pasa los datos
     }
